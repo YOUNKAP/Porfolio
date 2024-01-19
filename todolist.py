aca4739fd3b3873ba50f3f697bbb1958ad6d9ac9
YOUNKAP <nina.younkap@ccaa.pro>
2024-01-18 22:43:05 -0500

    initial

---
+++ todolist.py
@@ -0,0 +1,80 @@
+
+#0- Bon cours
+https://ocw.mit.edu/search/?q=postgis
+
+# 1-Geoserver
+https://www.youtube.com/playlist?list=PL_ITaxp1Ob4u5DBn_b8xSXjYcmbgYKgVu
+
+#Leaflet
+
+#2-GEE
+https://www.youtube.com/playlist?list=PLLW-qoCMKQsx62nKon2-0PMtGzkxlwN7k
+https://courses.spatialthoughts.com/gee-introduction.html
+
+#3-Figma design
+Udemy
+
+#4 Geoserver
+Udemy
+
+#Django - GeoDjango
+CSS50
+
+#LULC
+Planet + data
+
+#6 Project on floods
+https://colab.research.google.com/drive/1wmNeOXdVcEFYzpMpUxaPib1QUB0ksp8u#scrollTo=dzeHYa5GCxN7
+https://colab.research.google.com/drive/1Yx_JD-l6vKslv53nlEBFj9rkREN7HLrX#scrollTo=Ta8L-Y5F_v2r
+
+
+#7-Assessment test via upwork
+Upwork section messages when I am ready
+
+#8 - Teaching & Scientific paper
+CDE Scholarship
+Despite our numerous attempts, we were unable to produce reliable ground truth data.#
+
+#9- Update my cv on 4EI and Bayer
+
+# ALX
+## ORM
+https://sandtech.zoom.us/rec/share/oNjLdzVvuXFUmCytwfKaD1igGIUAT9529uKwpWQ6493USwss5r_VbSXAD06CO28r.u3Zjz_4F6UvsVSyx
+pwd : na7Y..rw 
+
+## DOVOP
+https://sandtech.zoom.us/rec/share/qRv8lIBfwkPGHwEPDs7GWv2iwVSJMXGaWOebA4FULKFD_mCCfd20zNQ0JR9JNgkD.4zVl1mCAWQIS0ZiS
+u0ybc#*X
+
+#SSH
+https://sandtech.zoom.us/rec/share/chDer0B4QuGfaK0nk_2QqARdqug7Fz4mv2JmLMf2LP7YZ7kyEQG0RtV3pA9AdNfF.Y-pEc1P0sUN_Ww5c
+8Z6ibmY+
+
+# Load Balancers:
+Recording - https://sandtech.zoom.us/rec/share/gPtVMy1cm5iUx7S_7QcMhep--snEb9AgwvDCzloh8FZVIeAWUkHoIDkPTKPr6IM.118umJU7RNYOxwBU
+Passcode: nFeS.Wu7
+
+
+#github student developer pack
+Zoom Recording: https://sandtech.zoom.us/rec/share/E7t3BAGbC5QEsmgGNpRQkrTw-cGX3QhqRvaUEJUyvOsmZ42qKr4_DgeHbmV2Fvmz.cT3ZnhPqD_UbAODT
+Passcode: k^%K2Kpd
+
+#From zero to funder
+https://www.youtube.com/watch?v=p99s748FoK8
+
+
+
+## DOWNLOAD COUNTRIS
+http://download.geofabrik.de/africa.html
+
+
+
+## ADS-B Exchange
+https://www.adsbexchange.com/data/
+
+## SPATIAL API
+https://web-mapping.surge.sh/setting-up-an-sql-api.html
+
+
+#github token
+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
